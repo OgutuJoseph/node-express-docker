e4a91ac2584dacf788883d6699878ac4ac53c407
OgutuJoseph <onyango.joseph@yahoo.com>
2023-02-13 16:54:38 +0300

    Communicating between containers

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 const port = process.env.PORT || 5001;
 
+
+/** Connecting to db */
+// mongoose.connect('mongodb://username:password@host:port/database?options...')
+
+// mongoose
+//     .connect('mongodb://root:password@172.18.0.2:27017/?authSource=admin')
+//     .then(() => console.log('Successfully connected to the database.'))
+//     .catch((e) => console.log(e))
+
+// pointing to the host / ip based on docker-compose configuration
+mongoose
+    .connect('mongodb://root:password@mongo:27017/?authSource=admin')
+    .then(() => console.log('Successfully connected to the mongo database.'))
+    .catch((e) => console.log(e))
+
+/** Routes */
 app.get('/', (req, res) => {
     // res.send('<h2>:: !! ::Node Docker:: !! :</h2>')
     // res.send(`
@@ -14,7 +31,7 @@ app.get('/', (req, res) => {
     res.send(`
     <h2>:: !! ::Node Docker:: !! ::</h2>
     <br />
-    <h4>Production...</h4>
+    <h4>Production</h4>
     `)
 })
 
